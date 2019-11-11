7c40999aa1084fe07d65c3a76b9783d75a8a6036
Joachim Neubert <j.neubert@zbw.eu>
2019-11-11 14:12:59 +0100

    Set language from filename

--- gen_pandoc.sh
+++ gen_pandoc.sh
@@ -13,7 +13,12 @@ output_file=$(dirname $input_file)/$(basename $input_file .md).html
 ##echo $output_file
 
 # language variable is used in the form of is_$lang in templates
-lang=de
+# (default language is de)
+if [[ $input_file == *.en.md ]]; then
+  lang=en
+else
+  lang=de
+fi
 
 pandoc --standalone --data-dir /disc1/pm20/web.public/pandoc --template pm20_default.html --css /styles/simple.css -t html+pipe_tables+fenced_divs+bracketed_spans --variable is_$lang --variable lang:$lang $input_file -o $output_file
 
