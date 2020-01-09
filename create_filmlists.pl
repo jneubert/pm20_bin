5cd6b66990e358c40c9711b129fae61a02177021
Joachim Neubert <j.neubert@zbw.eu>
2020-01-09 15:04:09 +0100

    Read ip hints from external file

--- create_filmlists.pl
+++ create_filmlists.pl
@@ -19,6 +19,7 @@ my $film_public_root = path('../web.public/film');
 my $filmdata_root    = path('../data/filmdata');
 ##my $filmdata_root    = $film_public_root;
 my $img_file         = $filmdata_root->child('img_count.json');
+my $ip_hints         = path('../web.public/templates/fragments/ip_hints.de.md')->slurp;
 
 my %page = (
   h => {
@@ -71,37 +72,6 @@ my %page = (
     },
   },
 );
-
-my $ip_hints = <<'EOF1';
-## Urheberrechte
-
-Der urheberrechtliche Status der auf den Filmen wiedergegebenen Artikel und
-sonstigen Publikationen ist von Werk zu Werk unterschiedlich. Die Prüfung des
-jeweiligen urheberrechlichen Status und die Einhaltung der gesetzlichen
-Bestimmungen - insbesondere was die Erstellung von Ausdrucken oder Kopien
-angeht - müssen wir den Benutzerinnen und Benutzern überlassen. Generell sind
-in Deutschland Publikationen 70 Jahre nach dem Tod des Autors / der Autorin
-(oder, bei anonymen/pseudonymen Publikationen, 70 Jahre nach der
-Veröffentlichung) gemeinfrei. Als Hilfestellung zur deutschen Gesetzeslage hier
-einige Links auf das [Gesetz über Urheberrecht und verwandte
-Schutzrechte](http://www.gesetze-im-internet.de/urhg/) (für das Ausland können
-die Regelungen abweichen):
-
-- [§ 2 Geschützte Werke](http://www.gesetze-im-internet.de/urhg/__2.html)
-- [§ 49 Zeitungsartikel und Rundfunkkommentare](http://www.gesetze-im-internet.de/urhg/__49.html)
-- [§ 51 Zitate](http://www.gesetze-im-internet.de/urhg/__51.html)
-- [§ 53 Vervielfältigungen zum privaten und sonstigen eigenen Gebrauch](http://www.gesetze-im-internet.de/urhg/__53.html)
-- [§ 60a Unterricht und Lehre](http://www.gesetze-im-internet.de/urhg/__60a.html)
-- [§ 60b Unterrichts- und Lehrmedien](http://www.gesetze-im-internet.de/urhg/__60b.html)
-- [§ 60c Wissenschaftliche Forschung](http://www.gesetze-im-internet.de/urhg/__60c.html)
-
-Bitte haben Sie Verständnis
-dafür, dass die Mitarbeiterinnen und Mitarbeiter der ZBW leider keine
-weitergehenden Rechtsauskünfte erteilen können.
-
-Ein eigenes Urheberrecht an den Digitalisaten erhebt die ZBW nicht.
-
-EOF1
 
 
 # TEMPORARY: remove path
