ec59148967f204a2f1b712e2816301ab41a0ee91
Joachim Neubert <j.neubert@zbw.eu>
2020-07-26 09:47:40 +0200

    Use vocab files, extend to English pages

--- create_category_pages.pl
+++ create_category_pages.pl
@@ -1,7 +1,8 @@
 #!/bin/env perl
 # nbt, 15.7.2020
 
-# create category overview pages from data/klassdata/*.json
+# create category overview pages from data/rdf/*.jsonld and
+# data/klassdata/*.json
 
 use strict;
 use warnings;
@@ -12,12 +13,13 @@ use Data::Dumper;
 use JSON;
 use Path::Tiny;
 use Readonly;
-use Scalar::Util qw(looks_like_number);
+use Scalar::Util qw(looks_like_number reftype);
 use YAML;
 
 my $web_root        = path('../web.public/category');
 my $klassdata_root  = path('../data/klassdata');
 my $folderdata_root = path('../data/folderdata');
+my $rdf_root        = path('../data/rdf');
 
 my %prov = (
   hwwa => {
@@ -28,7 +30,7 @@ my %prov = (
   },
 );
 
-my %subheading = (
+my %subheading_geo = (
   A => {
     de => 'Europa',
     en => 'Europe',
@@ -74,29 +76,25 @@ my $definitions_ref = YAML::Load(<<'EOF');
 geo:
   overview:
     title:
-      en: Folder Overview by Country Category System
-      de: Mappen-Übersicht nach Ländersystematik
+      en: Folders by Country Category System
+      de: Mappen nach Ländersystematik
     result_file: geo_by_signature
     output_dir: ../category/geo
     prov: hwwa
-    languages:
-      - de
-      - en
   single:
     result_file: subject_folders
     output_dir: ../category/geo/i
     prov: hwwa
-    languages:
-      - de
 EOF
 
-my %geo_lookup;
-my %subject_nta_lookup;
+my %geo                = get_vocab('ag');
+my %subject            = get_vocab('je');
+my %subheading_subject = get_subheadings( \%subject );
 
 # category overview pages
 foreach my $category_type ( keys %{$definitions_ref} ) {
   my $typedef_ref = $definitions_ref->{$category_type}->{overview};
-  foreach my $lang ( @{ $typedef_ref->{languages} } ) {
+  foreach my $lang (@languages) {
     my @lines;
     my $title      = $typedef_ref->{title}{$lang};
     my $provenance = $prov{ $typedef_ref->{prov} }{name}{$lang};
@@ -130,7 +128,7 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
       # control break?
       my $firstletter = substr( $category->{signature}->{value}, 0, 1 );
       if ( $firstletter ne $firstletter_old ) {
-        push( @lines, '', "### $subheading{$firstletter}{$lang}", '' );
+        push( @lines, '', "### $subheading_geo{$firstletter}{$lang}", '' );
         $firstletter_old = $firstletter;
       }
 
@@ -147,10 +145,6 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
       my $line =
         "- [$signature $label]" . "(i/$id/about.$lang.html) ($counter)";
       push( @lines, $line );
-
-      # lookup
-      $geo_lookup{$id}{signature} = $signature;
-      $geo_lookup{$id}{label}{$lang} = $label;
     }
 
     push( @lines, '', $backlink, '' );
@@ -163,7 +157,7 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
 # individual category pages
 foreach my $category_type ( keys %{$definitions_ref} ) {
   my $typedef_ref = $definitions_ref->{$category_type}->{single};
-  foreach my $lang ( @{ $typedef_ref->{languages} } ) {
+  foreach my $lang (@languages) {
     my $provenance = $prov{ $typedef_ref->{prov} }{name}{$lang};
 
     # read json input (all folders for all categories)
@@ -176,10 +170,6 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
     $file = $klassdata_root->child("subject_by_signature.$lang.json");
     my @subject_categories =
       @{ decode_json( $file->slurp )->{results}->{bindings} };
-    foreach my $subject_category (@subject_categories) {
-      $subject_nta_lookup{ $subject_category->{signature}->{value} } =
-        $subject_category->{categoryLabel}->{value};
-    }
 
     # main loop
     my @lines;
@@ -191,11 +181,17 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
 
       # TODO improve query to get values more directly
       $entry->{pm20}->{value} =~ m/(\d{6}),(\d{6})$/;
-      my $id1 = $1;
-      my $id2 = $2;
-      ## TODO look up signature form hash %signature (with ID as key)
-      my $signature = $entry->{subjectNta}->{value};
-      ( my $title, my $label ) = split( / : /, $entry->{pm20Label}->{value} );
+      my $id1   = $1;
+      my $id2   = $2;
+      my $title = "$geo{$id1}{notation} $geo{$id1}{prefLabel}{$lang}";
+      my $label = "$subject{$id2}{notation} ";
+      if ( $subject{$id2}{prefLabel}{$lang} ) {
+        $label .= $subject{$id2}{prefLabel}{$lang};
+      } else {
+
+        # temporary, German label in italics
+        $label .= "_$subject{$id2}{prefLabel}{de}_";
+      }
 
       # first level control break - new category page
       if ( $id1_old ne '' and $id1 ne $id1_old ) {
@@ -203,12 +199,10 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
         push( @output,
           '---',
           "title: \"$title_old\"",
-          "etr: category/$category_type/xxx",
+          "etr: category/$category_type/$geo{$id1_old}{notation}",
           '---', '' );
         push( @output, "## $provenance", '' );
-        push( @output,
-"# $geo_lookup{$id1_old}{signature} $geo_lookup{$id1_old}{label}{$lang}",
-          '' );
+        push( @output, "# $title_old",   '' );
         my $backlinktitle =
           $lang eq 'en'
           ? 'Back to Category Overview'
@@ -219,7 +213,6 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
         push( @output, $backlink, '', '' );
         @lines = ();
 
-
         my $out_dir =
           $web_root->child($category_type)->child('i')->child($id1_old);
         $out_dir->mkpath;
@@ -227,27 +220,89 @@ foreach my $category_type ( keys %{$definitions_ref} ) {
         $out->spew_utf8( join( "\n", @output ) );
         ## print join( "\n", @output, "\n" );
       }
-      $id1_old = $id1;
+      $id1_old   = $id1;
       $title_old = $title;
 
-      # second level control break
-      my $firstletter = substr( $signature, 0, 1 );
+      # second level control break (label starts with signature)
+      my $firstletter = substr( $label, 0, 1 );
       if ( $firstletter ne $firstletter_old ) {
 
-        # mainheading
-        my $main_heading = $subject_nta_lookup{$firstletter};
-        $main_heading =~ s/, Allgemein//;
-        push( @lines, '', "### $main_heading", '' );
+        # subheading
+        my $subheading = $subheading_subject{$firstletter}{$lang}
+          || $subheading_subject{$firstletter}{de};
+        $subheading =~ s/, Allgemein$//i;
+        $subheading =~ s/, General$//i;
+        push( @lines, '', "### $subheading", '' );
         $firstletter_old = $firstletter;
       }
 
       # main entry
       my $counter = $entry->{docs}->{value}
         . ( $lang eq 'en' ? ' documents' : ' Dokumente' );
-      my $line =
-        "- [$signature $label]" . "($entry->{pm20}->{value}) ($counter)";
+      my $line = "- [$label]" . "($entry->{pm20}->{value}) ($counter)";
       push( @lines, $line );
     }
   }
 }
 
+############
+
+sub get_vocab {
+  my $fn_stub = shift or die "param missing";
+
+  my %cat;
+  foreach my $lang (@languages) {
+    my $file = $rdf_root->child("$fn_stub.skos.jsonld");
+    my @categories =
+      @{ decode_json( $file->slurp )->{'@graph'} };
+
+    # read jsonld graph
+    foreach my $category (@categories) {
+
+      # skip category scheme and orphan entries
+      next unless $category->{'@type'} eq 'skos:Concept';
+      next unless exists $category->{broader};
+
+      my $id = $category->{identifier};
+      $cat{$id}{notation}     = $category->{notation};
+      $cat{$id}{notationLong} = $category->{notationLong};
+
+      foreach my $pref ( as_array( $category->{prefLabel} ) ) {
+        $cat{$id}{prefLabel}{ $pref->{'@language'} } = $pref->{'@value'};
+      }
+      foreach my $note ( as_array( $category->{scopeNote} ) ) {
+        $cat{$id}{scopeNote}{ $note->{'@language'} } = $note->{'@value'};
+      }
+    }
+  }
+  return %cat;
+}
+
+sub as_array {
+  my $ref = shift;
+
+  my @list = ();
+  if ($ref) {
+    if ( reftype($ref) eq 'ARRAY' ) {
+      @list = @{$ref};
+    } else {
+      @list = ($ref);
+    }
+  }
+  return @list;
+}
+
+sub get_subheadings {
+  my $hash_ref = shift or die "param missing";
+
+  my %subheading;
+  foreach my $cat ( keys %{$hash_ref} ) {
+    my $notation = $hash_ref->{$cat}{notation};
+    next unless $notation =~ m/^[a-z]$/;
+    foreach my $lang (@languages) {
+      $subheading{$notation}{$lang} =
+        $hash_ref->{$cat}{prefLabel}{$lang} || $hash_ref->{$cat}{prefLabel}{de};
+    }
+  }
+  return %subheading;
+}
