23e3e2396ae02b86c9f1949f5ac2314d8b556c15
Joachim Neubert <j.neubert@zbw.eu>
2021-01-11 13:17:05 +0100

    Changed paths (as of 2020-12-21)

--- get_image_lists.sh
+++ get_image_lists.sh
@@ -3,10 +3,5 @@
 
 # Get all images from the PM20 file systems - use "*_A.JPG" as indicator
 
-# VIA SMB SHARES, VERY TIME CONSUMING
-
-find /mnt/digidata/P -name "*_A.JPG" >  ../var/imagedata/pe_image.lst
-find /mnt/inst/F     -name "*_A.JPG" > ../var/imagedata/co_image.lst
-find /mnt/pers/A     -name "*_A.JPG" >> ../var/imagedata/co_image.lst
-find /mnt/sach/S     -name "*_A.JPG" >  ../var/imagedata/sh_image.lst
-find /mnt/ware/W     -name "*_A.JPG" >  ../var/imagedata/wa_image.lst
+find /pm20/folder/pe -name "*_A.JPG" >  /pm20/data/imagedata/pe_image.lst
+find /pm20/folder/sh -name "*_A.JPG" >  /pm20/data/imagedata/sh_image.lst
