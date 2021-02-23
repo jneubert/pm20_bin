ffdccac1950e9200c0532b0fc47fe223db0e16e2
Joachim Neubert <j.neubert@zbw.eu>
2021-02-23 11:19:49 +0100

    First complete version

--- results2markdown.pl
+++ results2markdown.pl
@@ -2,7 +2,10 @@
 # nbt, 2021-02-22
 
 # create markdown tables (for conversion to static html) from sparql results in
-# json
+# transmitted as json files
+# plus:
+# - recreate report directory symlink structure
+# - recreate about page
 
 use strict;
 use warnings;
@@ -12,75 +15,167 @@ use Data::Dumper;
 use JSON;
 use Path::Tiny;
 use Readonly;
-use Scalar::Util qw(looks_like_number);
+use YAML;
 
 binmode( STDOUT, ":utf8" );
 
-my $input_file = path('/tmp/persons_with_metadata.de.json');
+Readonly my $DEFINITIONS_FILE   => 'sparql_results.yaml';
+Readonly my $CONFIGURATION_FILE => 'reports.yaml';
+Readonly my $REPORT_ROOT        => path('/pm20/web.public/report');
 
-my $input = decode_json( $input_file->slurp );
+# read report definitions
+my %definition = %{ YAML::LoadFile($DEFINITIONS_FILE) };
+my %conf       = %{ YAML::LoadFile($CONFIGURATION_FILE) };
 
-# collect output lines, starting with page head
-my @lines;
-push( @lines,
-  '---', "title: \"x | ZBW Pressearchive\"",
-  "etr: report",
-  "backlink: ./about.de.html",
-  "backlink-title: Auswertungen",
-  '---', '' );
-push( @lines, "# titel", '' );
+# iterate over reports
+foreach my $report ( keys %definition ) {
 
-# read table head with field names
-my @fields;
-foreach my $field ( @{ $input->{head}{vars} } ) {
+  # iterate over languages
+  foreach my $lang ( keys %{ $definition{$report}{title} } ) {
 
-  # skip Labels for URI fields
-  next if $field =~ m/Label$/;
+    # read input
+    ( my $input_dir = $definition{$report}{output_dir} ) =~ s|/var/|/data/|;
+    my $input_file = path("$input_dir/$report.$lang.json");
+    my $input      = decode_json( $input_file->slurp );
 
-  push( @fields, $field );
-}
+    # collect output lines, starting with page head
+    my @lines;
+    my $title = $definition{$report}{title}{$lang};
 
-# print table head
-push( @lines, '::: {.wikitable}', '' );
-push( @lines, join( '|', @fields ) );
-my @delims = map( '-', @fields );
-push( @lines, join( '|', @delims ) );
+    push( @lines,
+      '---',
+      "title: \"$conf{rep}{$lang}: $title | $conf{archive}{$lang}\"",
+      "etr: report",
+      "backlink: ../about.$lang.html",
+      "backlink-title: $conf{backlink_title}{$lang}",
+      '---',
+      '' );
+    push( @lines, "## $conf{subtitle}{$lang}", '' );
+    push( @lines, "# $title", '' );
 
-# iteratre over data entries
-my $data_ref = $input->{results}{bindings};
-foreach my $entry ( @{$data_ref} ) {
+    # read table head with field names
+    my @fields;
+    foreach my $field ( @{ $input->{head}{vars} } ) {
 
-  # iterate over fields
-  my @row;
-  foreach my $field (@fields) {
+      # skip Labels for URI fields
+      next if $field =~ m/Label$/;
 
-    # handle empty fields
-    if ( not $entry->{$field} or $entry->{$field}{value} eq '' ) {
-      push( @row, ' ' );
-      next;
+      push( @fields, $field );
     }
 
-    if ( $entry->{$field}{type} eq 'uri' ) {
+    # print table head
+    push( @lines, '::: {.wikitable}', '' );
+    push( @lines, join( '|', @fields ) );
+    my @delims = map( '-', @fields );
+    push( @lines, join( '|', @delims ) );
 
-      # handle URI fields
-      if ( my $text = $entry->{"${field}Label"}{value} ) {
-        push( @row, '[' . $text . '](' . $entry->{$field}{value} . ')' );
-      } else {
-        push( @row,
-              '['
-            . $entry->{$field}{value} . ']('
-            . $entry->{$field}{value}
-            . ')' );
+    # iteratre over data entries
+    my $data_ref = $input->{results}{bindings};
+    foreach my $entry ( @{$data_ref} ) {
+
+      # iterate over fields
+      my @row;
+      foreach my $field (@fields) {
+
+        # handle empty fields
+        if ( not $entry->{$field} or $entry->{$field}{value} eq '' ) {
+          push( @row, ' ' );
+          next;
+        }
+
+        if ( $entry->{$field}{type} eq 'uri' ) {
+
+          # handle URI fields
+          if ( my $text = $entry->{"${field}Label"}{value} ) {
+            push( @row, '[' . $text . '](' . $entry->{$field}{value} . ')' );
+          } else {
+            push( @row,
+                  '['
+                . $entry->{$field}{value} . ']('
+                . $entry->{$field}{value}
+                . ')' );
+          }
+        } else {
+
+          # handle other (literal) fields
+          push( @row, $entry->{$field}{value} );
+        }
       }
-    } else {
+      push( @lines, join( '|', @row ) );
+    }
 
-      # handle other (literal) fields
-      push( @row, $entry->{$field}{value} );
+    push( @lines, '', ':::', '' );
+
+    # output report in markdown
+    my $report_dir = $REPORT_ROOT->child( $definition{$report}{report_dir} );
+    my $md_file    = $report_dir->child("$report.$lang.md");
+    $md_file->spew_utf8( join( "\n", @lines ) );
+
+    # symlink json file from report dir (compute relative path)
+    my $json_file = $report_dir->child("$report.$lang.json");
+    $json_file->remove;
+    my $relpath = $input_file->realpath->relative($report_dir);
+    symlink( $relpath, $json_file );
+  }
+}
+
+# recreate about page
+
+# iterate over languages
+foreach my $lang (qw/de en/) {
+
+  my @lines;
+  my $title = $conf{backlink_title}{$lang};
+  push( @lines,
+    '---',
+    "title: \"$title | $conf{archive}{$lang}\"",
+    "etr: report",
+    "backlink: ../about.$lang.html",
+    "backlink-title: Home",
+    'fn-stub: about',
+    '---',
+    '',
+    "# $title",
+    '' );
+  push( @lines,
+    $lang eq 'de'
+    ? 'Ergebnisse von Abfragen der Pressearchiv-Datenbank (Metadaten über das ehemalige HWWA-Archiv).'
+    : 'Query results from the press archives database (metadata about the former HWWW archives).',
+    '' );
+
+  # iterate over page sections
+  foreach my $section ( @{ $conf{sections} } ) {
+
+    push( @lines, "## $conf{section}{$section}{$lang}", '' );
+
+    # iterate over reports
+    for my $report ( keys %definition ) {
+
+      next unless $definition{$report}{report_dir} eq $section;
+
+      if ( $definition{$report}{title}{$lang} ) {
+        my $title = $definition{$report}{title}{$lang};
+        ( my $main_title = $title ) =~ s/ /+/g;
+        my $json_file = "$section/$report.$lang.json";
+        my $report_link =
+            "https://pm20.zbw.eu/report/pm20_result.$lang.html?"
+          . "jsonFile=$json_file&main_title=$main_title";
+        my $html_file = "$section/$report.$lang.html";
+        push( @lines,
+          "* [$title]($report_link) "
+            . "<small>([html]($html_file), [json]($json_file))</small>",
+          '' );
+      }
     }
   }
-  push( @lines, join( '|', @row ) );
-}
+  my $note = $lang eq 'de'
+  ? 'Diese Daten sind auch über einen [SPARQL-Endpoint](http://zbw.eu/beta/sparql-lab/about#pm20) abfragbar. ' 
+  . 'Die Quelltexte der Abfragen sind über [Github](https://github.com/zbw/sparql-queries/tree/master/pm20) zugänglich.'
+  : 'This data is also queryable via a [SPARQL endpoint](http://zbw.eu/beta/sparql-lab/about#pm20).. ' 
+  . 'The source code of the queries is accessible on [Github](https://github.com/zbw/sparql-queries/tree/master/pm20).';
+  push(@lines, "<small>$note</small>");
 
-push( @lines, '', ':::', '' );
-print join( "\n", @lines );
+  # write output
+  $REPORT_ROOT->child("about.$lang.md")->spew_utf8( join( "\n", @lines ) );
+}
 
