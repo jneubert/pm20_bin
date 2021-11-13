86cc7d7266f49533419e7b896249210cabf666b2
Joachim Neubert <j.neubert@zbw.eu>
2021-11-13 12:25:35 +0100

    Adapt examples

--- extract_examples.sh
+++ extract_examples.sh
@@ -5,7 +5,7 @@
 jq '{
     "@context": {"@context"},
     "@graph":[
-      ."@graph"[] | select(.identifier as $a | ["pe/005823", "co/041389"] | index($a) )
+      ."@graph"[] | select(.identifier as $a | ["pe/005823", "co/041389", "sh/126128,144265", "wa/143120,141691" ] | index($a) )
     ]
-  }'
+  }' ../data/rdf/pm20.extended.jsonld
 
