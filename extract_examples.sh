bb86255375b577d17c5ccf10f844795c1c339bac
Joachim Neubert <j.neubert@zbw.eu>
2021-11-13 11:13:24 +0100

    Add extract_examples.sh (from cdv2021-pressemappe20 repo)

---
+++ extract_examples.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# extract example records from complete jsonld file
+
+jq '{
+    "@context": {"@context"},
+    "@graph":[
+      ."@graph"[] | select(.identifier as $a | ["pe/005823", "co/041389"] | index($a) )
+    ]
+  }'
+
