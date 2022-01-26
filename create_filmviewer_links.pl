322be1de7b861adb6c0c7103ff9690f9be72fef7
Joachim Neubert <j.neubert@zbw.eu>
2022-01-26 12:30:31 +0100

    Factor out parse_filmlist(); fix regex

--- create_filmviewer_links.pl
+++ create_filmviewer_links.pl
@@ -52,55 +52,62 @@ $vocab{geo}     = ZBW::PM20x::Vocab->new('ag');
 $vocab{subject} = ZBW::PM20x::Vocab->new('je');
 
 my %position;
+parse_filmlist($subset);
 
-my $filmlists_ref = decode_json( $FILMDATA_ROOT->child("$subset.json")->slurp );
+####################
 
-foreach my $entry ( @{$filmlists_ref} ) {
-  next if ( $entry->{online} ne '' );
+sub parse_filmlist {
+  my $subset = shift or die "param mssing";
 
-  # restrict to example A12/Polen
-  next if ( $entry->{film_id} lt 'S0220H_2' );
-  next if ( $entry->{film_id} gt 'S0236H_1' );
+  my @filmlist =
+    @{ decode_json( $FILMDATA_ROOT->child("$subset.json")->slurp ) };
 
-  POS:
-  foreach my $pos ( 'start', 'end' ) {
-    my $ord = $pos eq 'start' ? 0 : 9999;
+  foreach my $entry (@filmlist) {
+    next if ( $entry->{online} ne '' );
 
-    my %sig;
-    if ( $entry->{"${pos}_sig"} =~ m/\[(\S+)\s+(.*)?(?: - (.+))?\]$/ ) {
-      $sig{geo}     = $1;
-      $sig{subject} = $2;
-      ## save optional keyword for later use
-      $position{$ord}{keyword} = $3;
-    } else {
-      warn "cannot parse signatur of ", Dumper $entry;
-    }
+    # restrict to example A12/Polen
+    next if ( $entry->{film_id} lt 'S0220H_2' );
+    next if ( $entry->{film_id} gt 'S0236H_1' );
 
-    # substitute known variants in signatures
-    $sig{subject} =~ s/q sm/q Sm/;
-    $sig{subject} =~ s/q Nr /q Sm/;
+    POS:
+    foreach my $pos ( 'start', 'end' ) {
+      my $ord = $pos eq 'start' ? 0 : 9999;
 
-    foreach my $voc ( sort keys %sig ) {
-
-      my $id = $vocab{$voc}->lookup_signature( $sig{$voc} );
-      if ( not $id ) {
-        warn "$entry->{film_id}: $pos signature $sig{$voc} not found\n";
-        if ( $voc eq 'geo' ) {
-          die;
-        }
-        next POS;
+      my %sig;
+      if ( $entry->{"${pos}_sig"} =~ m/\[(\S+)\s+(.+?)(?: - (.+))?\]$/ ) {
+        $sig{geo}     = $1;
+        $sig{subject} = $2;
+        ## save optional keyword for later use
+        $position{$ord}{keyword} = $3;
+      } else {
+        warn "cannot parse signatur of ", Dumper $entry;
       }
-      $position{$ord}{$voc}{signature} = $sig{$voc};
-      $position{$ord}{$voc}{id}        = $id;
 
-      foreach my $lang (@LANGUAGES) {
-        $position{$ord}{$voc}{label}{$lang} = $vocab{$voc}->label( $lang, $id );
+      # substitute known variants in signatures
+      $sig{subject} =~ s/q sm/q Sm/;
+      $sig{subject} =~ s/q Nr /q Sm/;
+
+      foreach my $voc ( sort keys %sig ) {
+
+        my $id = $vocab{$voc}->lookup_signature( $sig{$voc} );
+        if ( not $id ) {
+          warn "$entry->{film_id}: $pos signature $sig{$voc} not found\n";
+          if ( $voc eq 'geo' ) {
+            die;
+          }
+          next POS;
+        }
+        $position{$ord}{$voc}{signature} = $sig{$voc};
+        $position{$ord}{$voc}{id}        = $id;
+
+        foreach my $lang (@LANGUAGES) {
+          $position{$ord}{$voc}{label}{$lang} =
+            $vocab{$voc}->label( $lang, $id );
+        }
       }
     }
   }
 }
-
-####################
 
 sub usage {
   print "usage: $0 { " . join( ' | ', @VALID_SUBSETS ) . " }\n";
