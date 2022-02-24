101b1fd81274f5b164d48147877f3285fdf27caa
Joachim Neubert <j.neubert@zbw.eu>
2022-02-24 10:31:30 +0100

    Include zotero eval and creation of filmviewer links (as of 2022-02-15)

--- recreate_filmlists.sh
+++ recreate_filmlists.sh
@@ -2,13 +2,19 @@
 # nbt, 2020-02-17
 
 # to be invoked from ite-srv24
+# now includes zotero eval and creation of filmviewer links
 
 BASE_DIR=/pm20
+LOG_DIR=$BASE_DIR/web/tmp/film_meta
 
 export PERL5LIB=/opt/perllib:/opt/perl5/lib/perl5
 
 cd $BASE_DIR/bin
-perl create_filmlists.pl > /dev/null
+perl create_filmlists.pl > $LOG_DIR/create_filmlists.log 2>&1
+
+SUBSET=h1_sh
+perl read_zotero.pl $SUBSET  > $LOG_DIR/read_zotero.$SUBSET.log 2>&1
+perl create_filmviewer_links.pl $SUBSET > $LOG_DIR/create_filmviewer_links.$SUBSET.log 2>&1
 
 cd $BASE_DIR/web
 make > /dev/null
