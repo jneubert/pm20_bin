f576a1b6381d46c44e643439a11ce62951428b29
Joachim Neubert <j.neubert@zbw.eu>
2022-03-07 19:58:06 +0100

    Interim

--- create_iiif_manifest.pl
+++ create_iiif_manifest.pl
@@ -1,44 +1,47 @@
 #!/bin/perl
 # nbt, 31.1.2018
 
-# create a IIIF manifest for pm20 folders
+# create a IIIF manifest files for pm20 folders
+
+# can be invoked either by
+# - an extended folder id (e.g., pe/000012)
+# - a collection id (e.g., pe)
+# - 'ALL' (to (re-) create all collections)
 
 use strict;
 use warnings;
+use utf8;
+
+use lib './lib';
 
 use Data::Dumper;
 use Encode;
-use HTML::Entities;
+use HTML::Entities qw(encode_entities_numeric);
 use HTML::Template;
 use JSON;
 use Path::Tiny;
+use Readonly;
+use ZBW::PM20x::Folder;
 
 $Data::Dumper::Sortkeys = 1;
 
-##my $pm20_root_uri = 'http://pm20.zbw.eu/folder/';
-my $pm20_root_uri   = 'https://pm20.zbw.eu/folder/';
-my $folder_root_uri = 'http://purl.org/pressemappe20/folder/';
-my $pdf_root_uri    = 'https://pm20.zbw.eu/pdf/folder/';
-my $iiif_root       = path('../web/folder/');
-##my $iiif_root       = path('/disc1/pm20/folder');
-my $imagedata_root  = path('../data/imagedata');
-my $docdata_root    = path('../data/docdata');
-my $folderdata_root = path('../data/folderdata');
-
-my %res_ext = (
+Readonly my $PM20_ROOT_URI   => 'https://pm20.zbw.eu/folder/';
+Readonly my $FOLDER_ROOT_URI => 'http://purl.org/pressemappe20/folder/';
+Readonly my $PDF_ROOT_URI    => 'https://pm20.zbw.eu/pdf/folder/';
+## manifest files exist in the web tree
+Readonly my $IIIF_ROOT       => path('../web/folder/');
+Readonly my $IMAGEDATA_ROOT  => path('../data/imagedata');
+Readonly my $DOCDATA_ROOT    => path('../data/docdata');
+Readonly my $FOLDERDATA_ROOT => path('../data/folderdata');
+Readonly my %RES_EXT         => (
   A => '_A.JPG',
   B => '_B.JPG',
   C => '_C.JPG',
 );
+Readonly my @LANGUAGES   => qw/ en de /;
+Readonly my @COLLECTIONS => qw/ co pe sh wa /;
 
 my %holding = (
-
-  #  test => {
-  #    prefix   => 'pe/',
-  #    url_stub => {
-  #      '/mnt/digidata/P' => 'http://webopac.hwwa.de/DigiPerson/P/',
-  #    },
-  #  },
   co => {
     type_label => 'Firma',
     prefix     => 'co/',
@@ -71,8 +74,9 @@ my %holding = (
   },
 );
 
-my $manifest_tmpl = HTML::Template->new(
+my $tmpl = HTML::Template->new(
   filename          => '../etc/html_tmpl/static_manifest.json.tmpl',
+  utf8              => 1,
   loop_context_vars => 1
 );
 
@@ -81,59 +85,113 @@ my (
   $docdata_ref,  $imagedata_ref,  $imagesize_ref,  $folderdata_ref
 );
 
-foreach my $collection ( sort keys %holding ) {
+# check arguments
+if ( scalar(@ARGV) == 1 ) {
+  if ( $ARGV[0] =~ m:^(co|pe|wa|sh)$: ) {
+    my $collection = $1;
+    mk_collection($collection);
+  } elsif ( $ARGV[0] =~ m:^(co|pe)/(\d{6}): ) {
+    my $collection = $1;
+    my $folder_nk  = $2;
+    mk_folder( $collection, $folder_nk );
+  } elsif ( $ARGV[0] =~ m:^(sh|wa)/(\d{6},\d{6})$: ) {
+    my $collection = $1;
+    my $folder_nk  = $2;
+    mk_folder( $collection, $folder_nk );
+  } elsif ( $ARGV[0] eq 'ALL' ) {
+    mk_all();
+  } else {
+    &usage;
+  }
+} else {
+  &usage;
+}
 
-  # TODO reactivate
-  next unless $collection eq 'co';
+####################
 
-  # load input files
-  $docdata_file    = $docdata_root->child("${collection}_docdata.json");
-  $docdata_ref     = decode_json( $docdata_file->slurp );
-  $imagedata_file  = $imagedata_root->child("${collection}_image.json");
-  $imagedata_ref   = decode_json( $imagedata_file->slurp );
-  $imagesize_file  = $imagedata_root->child("${collection}_size.json");
-  $imagesize_ref   = decode_json( $imagesize_file->slurp );
-  $folderdata_file = $folderdata_root->child("${collection}_label.json");
-  $folderdata_ref  = decode_json( $folderdata_file->slurp );
+sub mk_all {
 
-  foreach my $folder_id ( sort keys %{$docdata_ref} ) {
-
-    # TODO reactivate
-    next unless $folder_id eq "019784";
-
-    # skip if none of the folder's articles are free
-    next unless exists $docdata_ref->{$folder_id}{free};
-
-    my $label =
-      get_folderlabel( $folder_id, $holding{$collection}{type_label} );
-    my $folder_uri = "$folder_root_uri$holding{$collection}{prefix}$folder_id";
-    my $pdf_url =
-        $pdf_root_uri
-      . "$collection/"
-      . get_folder_relative_path($folder_id)
-      . "/${folder_id}.pdf";
-
-    # create iiif manifest
-    my %manifest_tmpl_var = (
-      folder_label => $label,
-      manifest_uri => "$pm20_root_uri$collection/$folder_id/manifest.json",
-      folder_uri   => $folder_uri,
-      main_loop    => build_canvases( $collection, $folder_id, $folder_uri ),
-    );
-    $manifest_tmpl->param( \%manifest_tmpl_var );
-    write_manifest( $collection, $folder_id, $manifest_tmpl );
-
-    # create url aliases for awstats
-    ##print $url_fh "/beta/pm20mets/$collection/"
-    ##  . get_folder_relative_path($folder_id)
-    ##  . "/${folder_id}.xml\t$label\n";
+  foreach my $collection (@COLLECTIONS) {
+    mk_collection($collection);
   }
 }
 
-## TODO reactivate
-##close($url_fh);
+sub mk_collection {
+  my $collection = shift or die "param missing";
 
-####################
+  # load input files
+  load_files($collection);
+
+  foreach my $folder_nk ( sort keys %{$imagedata_ref} ) {
+    mk_folder( $collection, $folder_nk );
+  }
+}
+
+sub mk_folder {
+  my $collection = shift || die "param missing";
+  my $folder_nk  = shift || die "param missing";
+
+  my $folder = ZBW::PM20x::Folder->new( $collection, $folder_nk );
+
+  # check if folder dir exists
+  my $rel_path  = $folder->get_folder_hashed_path();
+  my $full_path = $ZBW::PM20x::Folder::FOLDER_ROOT->child($rel_path);
+  if ( not -d $full_path ) {
+    die "$full_path does not exist\n";
+  }
+
+  # open files if necessary
+  # (check with arbitrary entry)
+  if ( not defined $imagedata_ref ) {
+    load_files($collection);
+  }
+
+  # TODO clearly wrong - change to public/intern pdfs?
+  my $pdf_url = $PDF_ROOT_URI . "$rel_path/${folder_nk}.pdf";
+
+  foreach my $type ( 'public', 'intern' ) {
+
+    # get document list, skip if empty
+    my $doclist_ref = $folder->get_doclist($type);
+    next unless $doclist_ref and scalar( @{$doclist_ref} ) gt 0;
+
+    foreach my $lang (@LANGUAGES) {
+      my $label = encode_entities_numeric( $folder->get_folderlabel($lang) );
+
+      # feedback mailto
+      my $mailto =
+          "&#109;&#97;ilto&#58;p%72essema%70pe&#50;0&#64;&#37;&#55;Ab%77&#46;eu"
+        . "?subject=Feedback%20zu%20PM20%20$label"
+        . "&amp;body=%0D%0A%0D%0A%0D%0A---%0D%0A"
+        . "https://pm20.zbw.eu/dfgview/$collection/$folder_nk";
+
+      my $folder_uri = $folder->get_folder_uri;
+      my %tmpl_var   = (
+        folder_label => $label,
+        manifest_uri => "$PM20_ROOT_URI$collection/$folder_nk/manifest.json",
+        folder_uri   => $folder_uri,
+        main_loop    => build_canvases( $collection, $folder_nk, $folder_uri ),
+##        mailto        => $mailto,
+      );
+      $tmpl->param( \%tmpl_var );
+
+      write_manifest( $type, $lang, $folder, $tmpl );
+    }
+  }
+}
+
+sub load_files {
+  my $collection = shift || die "param missing";
+
+  $docdata_file    = $DOCDATA_ROOT->child("${collection}_docdata.json");
+  $docdata_ref     = decode_json( $docdata_file->slurp );
+  $imagedata_file  = $IMAGEDATA_ROOT->child("${collection}_image.json");
+  $imagedata_ref   = decode_json( $imagedata_file->slurp );
+  $imagesize_file  = $IMAGEDATA_ROOT->child("${collection}_size.json");
+  $imagesize_ref   = decode_json( $imagesize_file->slurp );
+  $folderdata_file = $FOLDERDATA_ROOT->child("${collection}_label.json");
+  $folderdata_ref  = decode_json( $folderdata_file->slurp );
+}
 
 sub get_max_image_fn {
   my $folder_id = shift || die "param missing";
@@ -154,7 +212,7 @@ sub get_image_uri {
   my $doc_id     = shift || die "param missing";
   my $image_id   = shift || die "param missing";
 
-  return "$pm20_root_uri${collection}/${folder_id}/${doc_id}/${image_id}";
+  return "$PM20_ROOT_URI${collection}/${folder_id}/${doc_id}/${image_id}";
 }
 
 sub get_image_dir {
@@ -164,7 +222,7 @@ sub get_image_dir {
   my $image_id   = shift || die "param missing";
 
   my $image_dir =
-    $iiif_root->child($collection)->child($folder_id)->child($doc_id)
+    $IIIF_ROOT->child($collection)->child($folder_id)->child($doc_id)
     ->child($image_id);
   $image_dir->mkpath;
   return $image_dir;
@@ -184,7 +242,7 @@ sub get_image_real_url {
       $holding{$collection}{url_stub}{ $imagedata{root} }
     . $imagedata{docs}{$doc_id}{rp} . '/'
     . $page
-    . $res_ext{$res};
+    . $RES_EXT{$res};
 
   return $url;
 }
@@ -240,18 +298,15 @@ sub build_canvases {
 }
 
 sub write_manifest {
-  my $collection    = shift || die "param missing";
-  my $folder_id     = shift || die "param missing";
-  my $manifest_tmpl = shift || die "param missing";
+  my $type   = shift || die "param missing";
+  my $lang   = shift || die "param missing";
+  my $folder = shift || die "param missing";
+  my $tmpl   = shift || die "param missing";
 
-  my %docdata = %{ $docdata_ref->{$folder_id} };
-
-  my $json = $manifest_tmpl->output;
-
-  my $manifest_dir = $iiif_root->child($collection)->child($folder_id);
-  $manifest_dir->mkpath;
-  my $manifest_file = $manifest_dir->child('manifest.json');
-  $manifest_file->spew($json);
+  my $hashed_path   = $folder->get_folder_hashed_path();
+  my $manifest_dir  = $IIIF_ROOT->child($hashed_path);
+  my $manifest_file = $manifest_dir->child("$type.manifest.$lang.json");
+  $manifest_file->spew_utf8( $tmpl->output );
 }
 
 sub get_folderlabel {
@@ -324,7 +379,7 @@ sub convert_label {
   my $label = shift || die "param missing";
 
   $label = Encode::encode( 'utf-8', $label );
-  $label = encode_entities( $label, '<>&"' );
+  $label = encode_entities_numeric( $label, '<>&"' );
 
   return $label;
 }
@@ -341,5 +396,10 @@ sub get_folder_relative_path {
     last;
   }
   return $folder_relative_path;
+}
+
+sub usage {
+  print "Usage: $0 {folder-id}|{collection}|ALL\n";
+  exit 1;
 }
 
