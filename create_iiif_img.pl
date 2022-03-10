f29ae3643826fd87811c7fcf7e4f105822836e61
Joachim Neubert <j.neubert@zbw.eu>
2022-03-10 11:38:26 +0100

    Allow arguments

--- create_iiif_img.pl
+++ create_iiif_img.pl
@@ -39,64 +39,106 @@ my $info_tmpl =
 
 my ( $imagedata_file, $imagesize_file, $imagedata_ref, $imagesize_ref, );
 
-foreach my $collection (@COLLECTIONS) {
+# check arguments
+if ( scalar(@ARGV) == 1 ) {
+  if ( $ARGV[0] =~ m:^(co|pe|wa|sh)$: ) {
+    my $collection = $1;
+    mk_collection($collection);
+  } elsif ( $ARGV[0] =~ m:^(co|pe)/(\d{6}): ) {
+    my $collection = $1;
+    my $folder_nk  = $2;
+    mk_folder( $collection, $folder_nk );
+  } elsif ( $ARGV[0] =~ m:^(sh|wa)/(\d{6},\d{6})$: ) {
+    my $collection = $1;
+    my $folder_nk  = $2;
+    mk_folder( $collection, $folder_nk );
+  } elsif ( $ARGV[0] eq 'ALL' ) {
+    mk_all();
+  } else {
+    &usage;
+  }
+} else {
+  &usage;
+}
 
-  # TODO reactivate
-  next unless $collection eq 'co';
+####################
+
+sub mk_all {
+
+  foreach my $collection (@COLLECTIONS) {
+    mk_collection($collection);
+  }
+}
+
+sub mk_collection {
+  my $collection = shift or die "param missing";
 
   # load input files
-  $imagedata_file = $IMAGEDATA_ROOT->child("${collection}_image.json");
-  $imagedata_ref  = decode_json( $imagedata_file->slurp );
-  $imagesize_file = $IMAGEDATA_ROOT->child("${collection}_size.json");
-  $imagesize_ref  = decode_json( $imagesize_file->slurp );
+  load_files($collection);
 
   foreach my $folder_nk ( sort keys %{$imagedata_ref} ) {
+    mk_folder( $collection, $folder_nk );
+  }
+}
 
-    # TODO reactivate
-    next unless $folder_nk eq "019784";
-    my $folder = ZBW::PM20x::Folder->new( $collection, $folder_nk );
+sub mk_folder {
+  my $collection = shift || die "param missing";
+  my $folder_nk  = shift || die "param missing";
 
-    foreach my $doc_id ( keys %{ $imagedata_ref->{$folder_nk}{docs} } ) {
+  # open files if necessary
+  # (check with arbitrary entry)
+  if ( not defined $imagedata_ref ) {
+    load_files($collection);
+  }
 
-      foreach my $page ( @{ $imagedata_ref->{$folder_nk}{docs}{$doc_id}{pg} } )
-      {
-        my $max_image_fn = get_max_image_fn( $folder_nk, $doc_id, $page );
-        my $image_id     = substr( $page, 24, 4 );
-        my $image_uri =
-          get_image_uri( $collection, $folder_nk, $doc_id, $image_id );
-        my $image_dir =
-          get_image_dir( $collection, $folder_nk, $doc_id, $image_id );
+  my $folder = ZBW::PM20x::Folder->new( $collection, $folder_nk );
 
-        my @rewrites;
+  foreach my $doc_id ( keys %{ $imagedata_ref->{$folder_nk}{docs} } ) {
 
-        # create iiif info
-        my %info_tmpl_var = ( image_uri => $image_uri, );
-        foreach my $res ( keys %RES_EXT ) {
-          my ( $width, $height ) = get_dim( $max_image_fn, $res );
-          my $real_url = get_image_real_url( $folder, $doc_id, $page, $res );
-          $info_tmpl_var{"width_$res"}  = $width;
-          $info_tmpl_var{"height_$res"} = $height;
+    foreach my $page ( @{ $imagedata_ref->{$folder_nk}{docs}{$doc_id}{pg} } ) {
+      my $max_image_fn = get_max_image_fn( $folder_nk, $doc_id, $page );
+      my $image_id     = substr( $page, 24, 4 );
+      my $image_uri =
+        get_image_uri( $collection, $folder_nk, $doc_id, $image_id );
+      my $image_dir =
+        get_image_dir( $collection, $folder_nk, $doc_id, $image_id );
 
-          # add rewrite
-          push( @rewrites, { "full" => $real_url } ) if ( $res eq 'A' );
-          push( @rewrites, { "$width,$height" => $real_url } );
-          push( @rewrites, { "$width,"        => $real_url } );
-        }
-        $info_tmpl->param( \%info_tmpl_var );
-        write_info( $image_dir, $info_tmpl );
+      my @rewrites;
 
-        # make thumbnail
-        make_thumbnail( $max_image_fn, $image_dir )
-          unless -f "$image_dir/thumbnail.jpg";
+      # create iiif info
+      my %info_tmpl_var = ( image_uri => $image_uri, );
+      foreach my $res ( keys %RES_EXT ) {
+        my ( $width, $height ) = get_dim( $max_image_fn, $res );
+        my $real_url = get_image_real_url( $folder, $doc_id, $page, $res );
+        $info_tmpl_var{"width_$res"}  = $width;
+        $info_tmpl_var{"height_$res"} = $height;
 
-        # htaccess file
-        write_htaccess( $image_dir, \@rewrites );
+        # add rewrite
+        push( @rewrites, { "full"           => $real_url } ) if ( $res eq 'A' );
+        push( @rewrites, { "$width,$height" => $real_url } );
+        push( @rewrites, { "$width,"        => $real_url } );
       }
+      $info_tmpl->param( \%info_tmpl_var );
+      write_info( $image_dir, $info_tmpl );
+
+      # make thumbnail
+      make_thumbnail( $max_image_fn, $image_dir )
+        unless -f "$image_dir/thumbnail.jpg";
+
+      # htaccess file
+      write_htaccess( $image_dir, \@rewrites );
     }
   }
 }
 
-####################
+sub load_files {
+  my $collection = shift || die "param missing";
+
+  $imagedata_file = $IMAGEDATA_ROOT->child("${collection}_image.json");
+  $imagedata_ref  = decode_json( $imagedata_file->slurp );
+  $imagesize_file = $IMAGEDATA_ROOT->child("${collection}_size.json");
+  $imagesize_ref  = decode_json( $imagesize_file->slurp );
+}
 
 sub get_max_image_fn {
   my $folder_nk = shift || die "param missing";
@@ -195,3 +237,9 @@ sub write_htaccess {
   }
   close($fh);
 }
+
+sub usage {
+  print "Usage: $0 {folder-id}|{collection}|ALL\n";
+  exit 1;
+}
+
