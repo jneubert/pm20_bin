7ef23c3635ad54d7a39d25f4267af2b02beafd25
Joachim Neubert <j.neubert@zbw.eu>
2022-04-01 09:10:34 +0200

    Production version (as of 2022-03-30)

--- create_sitemap.pl
+++ create_sitemap.pl
@@ -1,18 +1,21 @@
 #!/bin/env perl
 # nbt, 2022-03-25
 
+# get an indexed xml sitemap of all pm20 pages
+
 use strict;
 use warnings;
 
 use utf8;
 
 use Data::Dumper;
+use Path::Tiny;
 use Web::Sitemap;
 
 $Data::Dumper::Sortkeys = 1;
 
 my $sm = Web::Sitemap->new(
-  output_dir => '/var/tmp',
+  output_dir => '/pm20/web',
 
   ### Options ###
 
@@ -24,8 +27,9 @@ my $sm = Web::Sitemap->new(
   # mark for grouping urls
   ##default_tag => 'my_tag',
 
-# add <mobile:mobile/> inside <url>, and appropriate namespace (Google standard)
-  mobile => 1,
+  # add <mobile:mobile/> inside <url>, and appropriate namespace (Google
+  # standard)
+  ##mobile => 1,
 
   # add appropriate namespace (Google standard)
   ##images      => 1,
@@ -39,14 +43,11 @@ my $sm = Web::Sitemap->new(
   # specify data input charset
   charset => 'utf8',
 
-##        move_from_temp_action => sub {
-##                my ($temp_file_name, $public_file_name) = @_;
-
-  # ...some action...
-  #
-  # default behavior is
-  # File::Copy::move($temp_file_name, $public_file_name);
-##        }
+  move_from_temp_action => sub {
+    my ( $temp_file_name, $public_file_name ) = @_;
+    File::Copy::move( $temp_file_name, $public_file_name );
+    chmod 0664, $public_file_name;
+  }
 
 );
 
@@ -59,8 +60,9 @@ my @main_url_list = (
 );
 ##$sm->add( \@main_url_list, tag => 'main' );
 
-# work through all sets used in make
-foreach my $set (qw/ default category co pe sh wa /) {
+# work through all sets used in make, get all HTML urls (from file system) and
+# add them
+foreach my $set (qw/ default category co pe sh wa pdf /) {
   my $url_list_ref = get_urls($set);
   $sm->add( $url_list_ref, tag => $set );
 }
@@ -76,8 +78,18 @@ print Dumper $sm;
 sub get_urls {
   my $set = shift or die "param missing";
 
+  my @temp;
+  if ( $set eq 'pdf' ) {
+    ## get pdf from about-pm20 only (not doc)
+    @temp = `cd /pm20/web ; find ./about-pm20 -name "*.pdf"`;
+  } elsif ( grep (/^$set$/, qw/ co pe sh wa /)) {
+    ## use prepared list of folders with documents
+    @temp = split(/\n/, path("/pm20/data/folderdata/${set}_for_sitemap.lst")->slurp);
+  } else {
+    ## get a list of .md files as used in make
+    @temp = `/bin/sh /pm20/web/mk/find_md.sh $set`;
+  }
   my $url_list_ref;
-  my @temp = `/bin/sh /pm20/web/mk/find_md.sh $set`;
   foreach my $line (@temp) {
     chomp($line);
     $line = substr( $line, 1, );
