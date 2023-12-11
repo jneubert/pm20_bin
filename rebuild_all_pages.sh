876ee9108480e22a653d89fcf2fa95bd8ca5750d
Joachim Neubert <j.neubert@zbw.eu>
2023-12-11 09:13:43 +0100

    Add rebuild_all_pages.sh

---
+++ rebuild_all_pages.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# nbt, 11.12.2023
+#
+# Rebuild all pages after changes in the data
+# (e.g., after execution of recreate_document_locks.pl for moving wall)
+#
+set -e
+
+# overview pages / lists / categories (in markdown)
+# (it is possible that new entries occur,
+# e.g. when new persons are added which had no documents before
+for set in "pe" "co" ; do
+  perl create_folder_list.pl $set
+done
+# creates overview and individual pages for geo, subject and ware
+# (used for collections sh and wa)
+perl create_category_pages.pl
+
+# page markdown and image viewer input for individual folders
+for set in "pe" "co" "sh" "wa" ; do
+  perl create_folder_pages.pl $set
+  perl create_iiif_manifest.pl $set
+  perl create_mets.pl $set
+done
+
+# recrate html pages
+./web_make_all.sh
+
