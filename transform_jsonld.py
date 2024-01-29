627822a8677d1724d230ac9d5b80c42f1aee633f
Joachim Neubert <j.neubert@zbw.eu>
2024-01-29 02:41:10 +0100

    Add transform_jsonld.py (from ite-srv24, as of 2022-08-08)

---
+++ transform_jsonld.py
@@ -0,0 +1,21 @@
+# nbt, 2021-04-20
+#
+# uses PyLD for transformation, which includes labels in the framed result
+
+from pyld import jsonld
+import json
+import sys
+
+# get the frame name as command line argument
+# referenced context file has to be pulled in via http/s url
+frame_fh = open('../etc/' + sys.argv[1] + '.jsonld').read()
+frame = json.loads(frame_fh)
+
+data_fh = open('../var/rdf/pm20.interim.jsonld').read()
+data = json.loads(data_fh)
+
+#result = jsonld.flatten(data)
+result = jsonld.frame(data, frame)
+
+print(json.dumps(result, indent=2))
+
