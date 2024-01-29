d77fba946c2af6e6497eea01aab3d6c63ebd17b6
Joachim Neubert <j.neubert@zbw.eu>
2024-01-29 14:15:09 +0100

    Adapt paths

--- transform_jsonld.py
+++ transform_jsonld.py
@@ -8,10 +8,10 @@ import sys
 
 # get the frame name as command line argument
 # referenced context file has to be pulled in via http/s url
-frame_fh = open('../etc/' + sys.argv[1] + '.jsonld').read()
+frame_fh = open('../web/schema/' + sys.argv[1] + '.jsonld').read()
 frame = json.loads(frame_fh)
 
-data_fh = open('../var/rdf/pm20.interim.jsonld').read()
+data_fh = open('../data/rdf/pm20.interim.jsonld').read()
 data = json.loads(data_fh)
 
 #result = jsonld.flatten(data)
